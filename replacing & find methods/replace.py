d9b5523675b65f1241eaf3e8bb4e4a266c024460
Anish Shukla <shuklaanish040@gmail.com>
2025-06-21 11:03:27 +0530

    py

---
+++ replacing & find methods/replace.py
@@ -0,0 +1,6 @@
+text="anish shukla"
+print(text.replace("anish", "ravi"))
+
+# find method
+text="anish shukla"
+print(text.find("u"))
--- strings/concat.py
+++ strings/concat.py
@@ -1,3 +1,4 @@
 game="cricket"
 ground="lords"
 print("i love playing", game ,  "at" , ground)
+
